87b067706c308d0ffe6d1915ec65467a85450098
hissingshark <git@hissingshark.co.uk>
2021-02-15 08:07:13 +0000

    Clean up debug prints and redundant glfw code

--- src/window_sdl2.cpp
+++ src/window_sdl2.cpp
@@ -22,8 +22,7 @@ SDL2GameWindow::SDL2GameWindow(const std::string& title, int width, int height, 
 
     currentGameWindow = this;
 
-    // TODO should SDL find the native resolution to set?
-    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_EVENTS) != 0) { // TODO could init other subsystems now
+    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_EVENTS) != 0) {
         SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
 //        return 1; // TODO is this how we handle errors? We cannot return from here
     }
@@ -33,37 +32,7 @@ SDL2GameWindow::SDL2GameWindow(const std::string& title, int width, int height, 
         printf("Could not create SDL window: %s\n", SDL_GetError());
 //        return 1;
     }
-// TODO context and surface creation - do these need to be shared externally?  I don't think so as the context is globally set
     glcontext = SDL_GL_CreateContext(window);
-
-/*
-    sdl2DefaultWindowHints();
-    if (api == GraphicsApi::OPENGL_ES2) {
-        sdl2WindowHint(SDL2_CLIENT_API, SDL2_OPENGL_ES_API);
-        sdl2WindowHint(SDL2_CONTEXT_CREATION_API, SDL2_EGL_CONTEXT_API);
-        sdl2WindowHint(SDL2_CONTEXT_VERSION_MAJOR, 2);
-        sdl2WindowHint(SDL2_CONTEXT_VERSION_MINOR, 0);
-    } else if (api == GraphicsApi::OPENGL) {
-        sdl2WindowHint(SDL2_CONTEXT_VERSION_MAJOR, 3);
-        sdl2WindowHint(SDL2_CONTEXT_VERSION_MINOR, 2);
-        sdl2WindowHint(SDL2_OPENGL_FORWARD_COMPAT, GL_TRUE);
-        sdl2WindowHint(SDL2_OPENGL_PROFILE, SDL2_OPENGL_CORE_PROFILE);
-    }
-    window = sdl2CreateWindow(width, height, title.c_str(), nullptr, nullptr);
-    sdl2SetWindowUserPointer(window, this);
-    sdl2SetFramebufferSizeCallback(window, _sdl2WindowSizeCallback);
-    sdl2SetCursorPosCallback(window, _sdl2CursorPosCallback);
-    sdl2SetMouseButtonCallback(window, _sdl2MouseButtonCallback);
-    sdl2SetScrollCallback(window, _sdl2ScrollCallback);
-    sdl2SetWindowCloseCallback(window, _sdl2WindowCloseCallback);
-    sdl2SetKeyCallback(window, _sdl2KeyCallback);
-    sdl2SetCharCallback(window, _sdl2CharCallback);
-    sdl2SetWindowFocusCallback(window, _sdl2WindowFocusCallback);
-    sdl2SetWindowContentScaleCallback(window, _sdl2WindowContentScaleCallback);
-    sdl2MakeContextCurrent(window);
-
-    setRelativeScale();
-*/
 }
 
 SDL2GameWindow::~SDL2GameWindow() {
@@ -105,23 +74,18 @@ printf("Done.\n");
 }
 
 void SDL2GameWindow::handleKeyboardEvent(SDL_KeyboardEvent *keyevent) {
-printf("DEBUG: ");
-
     KeyCode key = getKeyMinecraft(keyevent->keysym.sym);
 
     KeyAction action;
     if (keyevent->repeat) {
-printf("Held...");
         action = KeyAction::REPEAT;
     }
     else {
         switch (keyevent->state) {
             case SDL_PRESSED:
-printf("Pressed.");
                 action = KeyAction::PRESS;
                 break;
             case SDL_RELEASED:
-printf("Released.");
                 action = KeyAction::RELEASE;
                 break;
             default:
@@ -153,50 +117,8 @@ void SDL2GameWindow::setSwapInterval(int interval) {
     SDL_GL_SetSwapInterval(interval);
 }
 
-/*
-void SDL2GameWindow::_sdl2WindowSizeCallback(SDL2window* window, int w, int h) {
-    SDL2GameWindow* user = (SDL2GameWindow*) sdl2GetWindowUserPointer(window);
-    user->onWindowSizeChanged(w, h);
-}
-
-void SDL2GameWindow::_sdl2CursorPosCallback(SDL2window* window, double x, double y) {
-    SDL2GameWindow* user = (SDL2GameWindow*) sdl2GetWindowUserPointer(window);
-
-    if (sdl2GetInputMode(window, SDL2_CURSOR) == SDL2_CURSOR_DISABLED) {
-        double dx = (x - user->lastMouseX) * user->getRelativeScale();
-        double dy = (y - user->lastMouseY) * user->getRelativeScale();
-
-        user->onMouseRelativePosition(dx, dy);
-        user->lastMouseX = x;
-        user->lastMouseY = y;
-    } else {
-        x *= user->getRelativeScale();
-        y *= user->getRelativeScale();
-
-        user->onMousePosition(x, y);
-    }
-}
-
-void SDL2GameWindow::_sdl2MouseButtonCallback(SDL2window* window, int button, int action, int mods) {
-    SDL2GameWindow* user = (SDL2GameWindow*) sdl2GetWindowUserPointer(window);
-    double x, y;
-    sdl2GetCursorPos(window, &x, &y);
-
-    x *= user->getRelativeScale();
-    y *= user->getRelativeScale();
-
-    user->onMouseButton(x, y, button + (button > SDL2_MOUSE_BUTTON_3 ? 5 : 1), action == SDL2_PRESS ? MouseButtonAction::PRESS : MouseButtonAction::RELEASE);
-}
-
-void SDL2GameWindow::_sdl2ScrollCallback(SDL2window* window, double x, double y) {
-    SDL2GameWindow* user = (SDL2GameWindow*) sdl2GetWindowUserPointer(window);
-    double cx, cy;
-    sdl2GetCursorPos(window, &cx, &cy);
-    user->onMouseScroll(cx, cy, x, y);
-}
-
-*/
-// TODO one universally required function, but codes will need to be checked for SDL2 compatability
+// TODO fix QWERTY and numpad mapping.  ? use scancode instead of keysym?
+one universally required function, but codes will need to be checked for SDL2 compatability
 KeyCode SDL2GameWindow::getKeyMinecraft(int keyCode) {
     if (keyCode >= SDLK_F1 && keyCode <= SDLK_F12)
         return (KeyCode) (keyCode - SDLK_F1 + (int) KeyCode::FN1);
@@ -220,7 +142,6 @@ KeyCode SDL2GameWindow::getKeyMinecraft(int keyCode) {
         case SDLK_CAPSLOCK:
             return KeyCode::CAPS_LOCK;
         case SDLK_ESCAPE:
-printf("Esc ");
             return KeyCode::ESCAPE;
         case SDLK_PAGEUP:
             return KeyCode::PAGE_UP;
@@ -279,58 +200,3 @@ printf("Esc ");
         return (KeyCode) keyCode;
     return KeyCode::UNKNOWN;
 }
-
-/*
-void SDL2GameWindow::_sdl2KeyCallback(SDL2window* window, int key, int scancode, int action, int mods) {
-    SDL2GameWindow* user = (SDL2GameWindow*) sdl2GetWindowUserPointer(window);
-#ifdef __APPLE__
-    if (action == SDL2_PRESS && mods & SDL2_MOD_SUPER && key == SDL2_KEY_V) {
-#else
-    if (action == SDL2_PRESS && mods & SDL2_MOD_CONTROL && key == SDL2_KEY_V) {
-#endif
-        user->onPaste(sdl2GetClipboardString(window));
-    }
-    if (action == SDL2_PRESS || action == SDL2_REPEAT) {
-        if (key == SDL2_KEY_BACKSPACE)
-            user->onKeyboardText("\x08");
-        if (key == SDL2_KEY_ENTER)
-            user->onKeyboardText("\n");
-    }
-    KeyAction enumAction = (action == SDL2_PRESS ? KeyAction::PRESS :
-                            (action == SDL2_REPEAT ? KeyAction::REPEAT : KeyAction::RELEASE));
-    auto minecraftKey = getKeyMinecraft(key);
-    if (key != SDL2_KEY_UNKNOWN && minecraftKey != KeyCode::UNKNOWN) {
-        user->onKeyboard(minecraftKey, enumAction);
-    } else {
-        if (!user->warnedButtons) {
-            user->warnedButtons = true;
-            GameWindowManager::getManager()->getErrorHandler()->onError("SDL2 Unknown Key", "Please check your Keyboard Layout. Falling back to scancode for unknown Keys.");
-        }
-        user->onKeyboard((KeyCode) scancode, enumAction);
-    }
-
-}
-
-void SDL2GameWindow::_sdl2CharCallback(SDL2window* window, unsigned int ch) {
-    SDL2GameWindow* user = (SDL2GameWindow*) sdl2GetWindowUserPointer(window);
-    std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> cvt;
-    user->onKeyboardText(cvt.to_bytes(ch));
-}
-
-void SDL2GameWindow::_sdl2WindowCloseCallback(SDL2window* window) {
-    SDL2GameWindow* user = (SDL2GameWindow*) sdl2GetWindowUserPointer(window);
-    sdl2SetWindowShouldClose(window, SDL2_FALSE);
-    user->onClose();
-}
-
-void SDL2GameWindow::_sdl2WindowFocusCallback(SDL2window* window, int focused) {
-    SDL2GameWindow* user = (SDL2GameWindow*) sdl2GetWindowUserPointer(window);
-    SDL2JoystickManager::onWindowFocused(user, focused == SDL2_TRUE);
-    user->focused = (focused == SDL2_TRUE);
-}
-
-void SDL2GameWindow::_sdl2WindowContentScaleCallback(SDL2window* window, float scalex, float scaley) {
-    SDL2GameWindow* user = (SDL2GameWindow*) sdl2GetWindowUserPointer(window);
-    user->setRelativeScale();
-}
-*/
